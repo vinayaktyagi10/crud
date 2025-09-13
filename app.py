4b76a65c6fadd8e0116327c21bbc1723372e0649
Vinayak Tyagi <vinayaktyagi.ed@gmail.com>
2025-09-13 17:36:12 +0530

    Add login endpoint

--- app.py
+++ app.py
@@ -18,13 +18,28 @@ def add_user():
         user_id = data["id"]
         password = data["password"]
         if users_collection.find_one({"id": user_id}):
-            return jsonify({"error": "User exists"}), 409
+            return jsonify({"error": "User already exists"}), 409
 
         users_collection.insert_one({"id": user_id, "password": password})
 
         return jsonify({"message": "User registered"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+@app.route("/api/login", methods=["GET", "POST"])
+def login():
+    data = request.get_json()
+    if not data or "id" not in data or "password" not in data:
+        return jsonify({"error": "Wrong 'id' or 'password'"}), 400
+    user_id = data["id"]
+    password = data["password"]
+    user = users_collection.find_one({"id": user_id})
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    if user["password"] != password:
+        return jsonify({"error": "Incorrect password"}), 401
+    return jsonify({"message": "User logged in"}), 200
 
 
 # ------------------- MAIN -------------------
